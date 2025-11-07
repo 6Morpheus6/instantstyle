3ec52c94bb48f52c6375e4db2584d78d0a564aca
6Morpheus6 <wanton-decay@gmx.net>
2025-11-07 02:13:00 +0100

    use hf-xet

--- install.js
+++ install.js
@@ -12,7 +12,10 @@ module.exports = {
       params: {
         venv: "env",
         path: "app",
-        message: "uv pip install -r requirements.txt"
+        message: [
+          "uv pip install -r requirements.txt",
+          "uv pip install hf-xet"
+        ]
       }
     },
     {
