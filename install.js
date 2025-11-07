29369b1ed936997664b7dd4440fd3a3db6fd03c6
6Morpheus6 <wanton-decay@gmx.net>
2025-11-07 01:55:54 +0100

    use uv
    - use uv
    - remove fs.link
    - remove script

--- install.js
+++ install.js
@@ -1,42 +1,43 @@
-const config = require("./config.js")
-const pre = require("./pre.js")
-module.exports = async (kernel) => {
-  let script = {
-    run: [{
+
+module.exports = {
+  run: [
+    {
       method: "shell.run",
       params: {
-        message: [
-          "git clone https://huggingface.co/spaces/cocktailpeanut/InstantStyle app",
-        ]
+        message: "git clone https://huggingface.co/spaces/cocktailpeanut/InstantStyle app"
       }
-    }, {
+    },
+    {
       method: "shell.run",
       params: {
         venv: "env",
         path: "app",
-        message: "pip install -r requirements.txt"
+        message: "uv pip install -r requirements.txt"
       }
-    }, {
+    },
+    {
       method: "shell.run",
       params: {
         path: "app",
         message: "python ipadapter.py"
       }
-    }, {
-      method: "fs.share",
+    },
+    {
+      method: "script.start",
       params: {
-        venv: "app/env"
+        uri: "torch.js",
+        params: {
+          venv: "env",
+          path: "app",
+          // xformers: true
+        }
       }
-    }, {
+    },
+    {
       method: "notify",
       params: {
         html: "Click the 'start' tab to get started!"
       }
-    }]
-  }
-  let pre_command = pre(config, kernel)
-  if (pre_command) {
-    script.run[1].params.message = [pre_command].concat(script.run[1].params.message)
-  }
-  return script
+    }
+  ]
 }
