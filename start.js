1d83621a26bf294a2e73d2dae244ed54c2422f3a
6Morpheus6 <wanton-decay@gmx.net>
2025-11-07 01:57:00 +0100

    remove local sharing
    - remove proxy.start
    - remove script

--- start.js
+++ start.js
@@ -1,29 +1,24 @@
-module.exports = async (kernel) => {
-  let script = {
-    daemon: true,
-    run: [{
+module.exports = {
+  daemon: true,
+  run: [
+    {
       method: "shell.run",
       params: {
         path: "app",
         venv: "env",
         env: { },
-        message: [
-          "python app.py",
-        ],
-        on: [{ "event": "/http:\/\/[0-9.:]+/", "done": true }]
+        message: "python app.py",
+        on: [{
+          event: "/http:\/\/[0-9.:]+/",
+          done: true
+        }]
       }
-    }, {
-      "method": "local.set",
-      "params": {
-        "url": "{{input.event[0]}}"
+    },
+    {
+      method: "local.set",
+      params: {
+        url: "{{input.event[0]}}"
       }
-    }, {
-      "method": "proxy.start",
-      "params": {
-        "uri": "{{local.url}}",
-        "name": "Local Sharing"
-      }
-    }]
-  }
-  return script
+    }
+  ]
 }
